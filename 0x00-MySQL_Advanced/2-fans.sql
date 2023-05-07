5cf8656889a6a1a79277e1b3360c761c86e5a264
koredeycode <koredey4u@gmail.com>
2023-05-07 16:49:20 +0000

    God abeg

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- ranks country origin of bands, ordered by the number of (non -unique) fans
 
-SELECT origin, SUM(fans) as nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM `metal_bands`
 GROUP BY origin
 ORDER BY nb_fans DESC;
--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,7 @@
 -- ranks country origin of bands,
--- ordered by the 
--- number of (non -unique) fans
+-- ordered by the number of (non -unique) fans
 
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
 FROM `metal_bands`
 WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY lifespan DESC
+ORDER BY lifespan DESC;
