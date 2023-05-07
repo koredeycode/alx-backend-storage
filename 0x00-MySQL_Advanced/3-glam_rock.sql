923c6cbd4ca1c813c4c00e6771af430657be1a41
koredeycode <koredey4u@gmail.com>
2023-05-07 16:38:04 +0000

    corrected

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- ordered by the 
 -- number of (non -unique) fans
 
-SELECT band_name, (IFNULL(split, '2020') - formed) as lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
 FROM `metal_bands`
 WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC
