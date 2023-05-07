8c9f11e158e4ca7441ea3b7cbf3e0fa42379cbc8
koredeycode <koredey4u@gmail.com>
2023-05-07 17:04:30 +0000

    done

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,15 @@
+ -- create trigger
+
+DROP TRIGGER IF EXISTS purchase;
+DELIMITER $$
+CREATE TRIGGER purchase
+AFTER INSERT
+ON orders
+FOR EACH ROW
+BEGIN
+	-- trigger code
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE name = NEW.item_name;
+END $$
+DELIMITER ;
