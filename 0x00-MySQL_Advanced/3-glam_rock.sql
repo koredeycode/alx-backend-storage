06d7cd10766ec1cc5b9fdf1df54f1e37b34cc92e
koredeycode <koredey4u@gmail.com>
2023-05-07 20:19:47 +0000

    last

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,6 @@
--- ranks country origin of bands,
--- ordered by the number of (non -unique) fans
-
-SELECT band_name, ABS(IFNULL(split, 2020) - formed) AS lifespan
-FROM `metal_bands`
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+-- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT band_name,
+ABS(IFNULL(split, 2022) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%' 
 ORDER BY lifespan DESC;
