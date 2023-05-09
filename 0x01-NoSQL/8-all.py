1d332c8fcda4e17a03687e13321e518821f46a51
koredeycode <koredey4u@gmail.com>
2023-05-09 15:13:36 +0000

    this

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Contains the list_all function
+"""
+
+def list_all(mongo_collection):
+    """
+    List all the documents in a collection
+    """
+    return [doc for doc in mongo_collection.find()]
+
