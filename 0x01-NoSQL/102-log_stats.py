77f4ea6d0cd86cb6033bb52d7c8ad57d33009cba
koredeycode <koredey4u@gmail.com>
2023-05-09 23:41:09 +0000

    this

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" 8-main """
+from pymongo import MongoClient
+list_all = __import__('8-all').list_all
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    log_number = nginx_collection.count_documents({})
+    print("{} logs".format(log_number))
+    print("Methods:")
+    stats_check = nginx_collection.count_documents({"path": "/status"})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        method_count = nginx_collection.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+    print("{} status check".format(stats_check))
+    print("IPs:")
+    logs = nginx_collection.aggregate(
+        [
+            {
+                '$group': {'_id': "$ip", 'totalRequests': {'$sum': 1}}
+            },
+            {
+                '$sort': {'totalRequests': -1}
+            },
+            {
+                '$limit': 10
+            },
+        ]
+    )
+    for log in logs:
+        ip = log["_id"]
+        ip_count = log['totalRequests']
+        print("\t{}: {}".format(ip, ip_count))
